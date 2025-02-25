583c3607d703aa0a3242fb3e63c5fd415518a78a
Jariel Balberona <jarielbalb@gmail.com>
2025-02-25 13:11:44 +0800

    add base layout initial

---
+++ app/src/config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "Freediving Philippines",
+  url: "http://freediving.ph/",
+  ogImage: "http://freediving.ph/og.jpg",
+  description:
+    "Revolutionizing the World of Freediving in the Philippines",
+  links: {
+    twitter: "https://twitter.com/freediving.ph",
+    github: "https://github.com/jarielbalberona/freediving.ph",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+}
