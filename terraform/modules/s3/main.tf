4acc3732fb8f20daa9a7cff9472780b5209349c8
Jariel Balberona <jarielbalb@gmail.com>
2025-03-02 07:32:08 +0800

    add s3 module

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -5,21 +5,24 @@ resource "aws_s3_bucket" "media_bucket" {
   bucket = "${var.aws_s3_bucket_name}-media"
 }
 
-# S3 Bucket Policy to Allow Presigned URL Uploads
-resource "aws_s3_bucket_policy" "media_policy" {
-  bucket = aws_s3_bucket.media_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.media_bucket.arn}/*",
-      }
-    ]
-  })
-}
+# # S3 Bucket Policy to Allow Presigned URL Uploads
+# resource "aws_s3_bucket_policy" "media_policy" {
+#   bucket = aws_s3_bucket.media_bucket.id
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect    = "Allow",
+#         Principal = "*",
+#         Action = [
+#           "s3:GetObject",
+#           "s3:PutObject"
+#         ],
+#         Resource = "${aws_s3_bucket.media_bucket.arn}/*",
+#       }
+#     ]
+#   })
+# }
 
 # CloudFront Distribution
 resource "aws_cloudfront_distribution" "cdn" {
